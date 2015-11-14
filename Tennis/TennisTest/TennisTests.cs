02b7354a955d20df5be58684a897399949dfcb05
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2015-11-14 14:46:31 +0700

    Tennis GetScore should be "LOVE-LOVE" when input a is 0 and b is 0. Tennis GetScore should be "15-LOVE" when input is a is 1 and b is 0.

---
+++ Tennis/TennisTest/TennisTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Tennis;
+
+namespace TennisTest
+{
+    [TestFixture]
+    public class TennisTests
+    {
+        [Test]
+        public void GetScore_Shouldbe_LOVE_LOVE_When_Input_0_0()
+        {
+            Assert.AreEqual("LOVE-LOVE", Tennis.Tennis.GetScore(0, 0));
+        }
+
+        [Test]
+        public void GetScore_Shouldbe_15_LOVE_When_Input_1_0()
+        {
+            Assert.AreEqual("15-Love",Tennis.Tennis.GetScore(1,0));
+        }
+    }
+}
